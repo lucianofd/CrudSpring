8582ee9d66bdaed9c44c836867c3662bf2b75b8e
lucianoFD <dlffuhr@gmail.com>
2022-06-19 17:58:23 -0300

    defino paquetes y clases para implementacion JWT

---
+++ src/main/java/com/example/crudSpring/security/enums/RolNombre.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.example.crudSpring.security.enums;
+
+/**
+ *
+ * @author Romina Chiti
+ */
+public enum RolNombre {
+    
+}
