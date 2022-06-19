c7f78609694c6e3bb4272d1f828915151c896726
lucianoFD <dlffuhr@gmail.com>
2022-06-19 19:02:31 -0300

    desarrollo entity usuario y enums

--- src/main/java/com/example/crudSpring/security/enums/RolNombre.java
+++ src/main/java/com/example/crudSpring/security/enums/RolNombre.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
+
 package com.example.crudSpring.security.enums;
 
-/**
- *
- * @author Romina Chiti
- */
+
 public enum RolNombre {
-    
+    ROLE_ADMIN, ROLE_USER
 }
