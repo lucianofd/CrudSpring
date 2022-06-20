9261fba63b278ccd3f0bb90cef293b1109c7c6e0
lucianoFD <dlffuhr@gmail.com>
2022-06-20 20:09:51 -0300

    desarrollo interfaces de reposiitory

--- src/main/java/com/example/crudSpring/security/repository/RolRepository.java
+++ src/main/java/com/example/crudSpring/security/repository/RolRepository.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package com.example.crudSpring.security.repository;
 
-/**
- *
- * @author Romina Chiti
- */
-public interface RolRepository {
+import com.example.crudSpring.security.entity.Rol;
+import com.example.crudSpring.security.enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface RolRepository extends JpaRepository<Rol, Integer>{
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+    
     
 }
--- src/main/java/com/example/crudSpring/security/repository/UsuarioRepository.java
+++ src/main/java/com/example/crudSpring/security/repository/UsuarioRepository.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package com.example.crudSpring.security.repository;
 
-/**
- *
- * @author Romina Chiti
- */
-public interface UsuarioRepository {
+import com.example.crudSpring.security.entity.Usuario;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+ 
+    Optional<Usuario> finByNombreUsuario(String nombreUsuario);
+    boolean existByNombreUsuario(String nombreUsuario);
+    boolean existByEmail(String email);
     
 }
