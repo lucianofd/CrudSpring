caa57a56a8185777a528efe33f7f17d3957e01d7
lucianoFD <dlffuhr@gmail.com>
2022-06-20 19:23:01 -0300

    defino entity UsuarioAdmin y Rol

--- src/main/java/com/example/crudSpring/security/entity/UsuarioAdmin.java
+++ src/main/java/com/example/crudSpring/security/entity/UsuarioAdmin.java
@@ -1,13 +1,86 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.example.crudSpring.security.entity;
 
-/**
- *
- * @author Romina Chiti
- */
-public class UsuarioAdmin {
+package com.example.crudSpring.security.entity;
+//genera seguridad, implementa privilegios de usuario
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+public class UsuarioAdmin implements UserDetails  {
+    
+    private String nombre;
+    private String nombreUsuario;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;  
+
+    public UsuarioAdmin(String nombre, String nombreUsuario, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.nombre = nombre;
+        this.nombreUsuario = nombreUsuario;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+    
+    //Como vamos a asignar privilegios, el build
+    //devuelvo usuario principal, 
+    // obtengo roles-los mapeo- accedo desde nombre del rol-lo paso a listado
+    //paso clase rol a grantedauthority
+    //devulevo usuario principal.
+    
+    public static UsuarioAdmin build(Usuario usuario){
+        List<GrantedAuthority> authorities =
+                usuario.getRoles().stream().map(rol -> new SimpleGrantedAuthority(rol.getRolNombre().name())).collect(Collectors.toList());
+        return new UsuarioAdmin (usuario.getNombre(), usuario.getNombreUsuario(), usuario.getEmail(), usuario.getPassword(), authorities);
+    }
+    
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities; // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return nombreUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    
+
+    @Override
+    public boolean isAccountNonExpired() {
+       return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+       return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
     
 }
